01472aedbabc3a3b70c3ed599d8b77ff9d84a301
Tiago Augusto <tiagoasrodrigues@protonmail.com>
2022-02-23 12:58:33 -0300

    :tada: Initial template

---
+++ src/styles/theme.ts
@@ -0,0 +1,11 @@
+const theme = {
+  colors: {
+    background: '#121214',
+    backgroundLight: '#FFFFFF',
+    text: '#121214',
+    textLight: '#e1e1e6',
+    primary: '#8257e6'
+  }
+};
+
+export default theme;
